cc5a891c321e50540245450683e9131cb7a8746e
Mwela Rachidi Jackson <rachidimwela@gmail.com>
2020-12-24 10:26:41 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,113 @@
+<div id="footer">
+
+<div class="container">
+    <div class="row">
+    <div class="col-sm-6 col-md-3">
+        <h3>Pages</h3>
+       <ul>
+          
+        <li><a href="cart.php"> Check Tickets</a></li>
+   
+           <li><a href="contact.php">Contacter nous</a></li>
+                <li><a href="shop.php">Reserver Maintenant</a></li>
+           <li><a href="customer/myaccount.php">Mon Compte</a></li>
+           
+        </ul> 
+        <h3>Section Utilisateur</h3>
+       <ul>
+          
+        <li><a href="login.php">Se Connecter</a></li>
+   
+           <li><a href="customer_register.php">S'enregistrer</a></li>
+          
+           
+        </ul> 
+        
+        <hr class="hidden-md hidden-lg hidden-sm">
+        
+        
+         
+        </div>
+        <div class="col-sm-6 col-md-3">
+        <h3>Services Categories </h3>
+        <ul>
+            
+              <li><a href="">Promotions</a></li>
+              <li><a href="">Acheter une Voiture</a></li>
+            <li><a href="">Voyage</a></li>
+              <li><a href="">Reserver un Vol</a></li>
+              <li><a href="">Reserver un Bateau</a></li>
+   
+            
+            
+            </ul>
+          
+            
+        <hr class="hidden-md hidden-lg hidden-sm">
+        
+            
+        
+        </div>
+        <div class="col-sm-6 col-md-3">
+        
+        <h4>Retrouve Nous:</h4>
+        <p>
+           <strong>SkyMag  Inc.</strong> 
+            <br/>Goma
+            <br/>Commune de Goma
+            <br/>0991337850
+            <br/>Skymago1@gmail.com
+            <br/><strong>Promesse</strong>
+            
+            
+         </p>
+        <a href="Contact.php">Regarder notre Page</a>
+            <hr class="hidden-md hidden-lg">
+            
+        </div>
+        <div class="col-sm-6 col-md-3">
+        <h4>Avoir nos Nouvelles</h4>
+        <p class="text-mute">
+        Pour etre toujours au courant des nos offres et publications
+            
+            </p>
+            <form action="" method="post">
+            <div class="input-group">
+                
+                <input type="text" class="form-control" name="email">
+                <span class="input-group-btn">
+                <input type="submit"  value="subscribe "class="btn btn-default">
+                </span>
+                
+                
+                </div>
+            </form>
+            <hr>
+            
+            <h4> Rester en Contact </h4>
+            <p class="social">
+            <a href="#" class="fa fa-facebook"></a>
+               <a href="#" class="fa fa-twitter"></a>
+               <a href="#" class="fa fa-Instagram"></a>
+               <a href="#" class="fa fa-google-plus"></a>
+               <a href="#" class="fa fa-envelope"></a>
+            </p>
+        </div>
+    
+    </div>
+    </div>
+</div><!-  Footer code-->
+<div id="Copyright"> 
+
+<div class="container">
+    
+<div class="col-md-6">
+    <p class="pull-left">@Copy; 2020 SkyMag inc.Tout droit Reserver </p>
+    
+    </div> 
+    <div class="col-md-6">
+    <p class="pull-right">Designed by:<a href="">Promesse</a></p>
+    
+    </div>  
+    </div>
+</div>
---
+++ includes/sidebar.php
@@ -0,0 +1,29 @@
+<div class="panel panel-default sidebar-menu">
+
+
+<div class="panel-heading">
+    
+    <h3 class="panel-title">Service Categorie </h3>
+    
+    </div>
+    
+    <div class="panel-body">
+    
+    <ul class="nav nav-pills nav-stacked category-menu">
+         <li><a href="#">Promotion</a></li>
+         <li><a href="#">Acheter une Voiture</a></li>
+         <li><a href="#">Voyager</a></li>
+      
+         <li><a href="#">Reserver un VoL</a></li>
+         <li><a href="#">Reserver un Bateau</a></li>
+        <li><a href="#">Reserver un Bus</a></li>
+        </ul>
+    </div>
+    
+    
+</div>
+<div class="panel panel-default sidebar-menu">
+
+
+    
+</div>
